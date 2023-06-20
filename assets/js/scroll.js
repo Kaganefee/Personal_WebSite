d36d2906455e8a55df8c601c0969fe084b60f2ec
kağan efe <106731838+Kaganefee@users.noreply.github.com>
2023-06-20 12:09:47 +0300

    first commit

---
+++ assets/js/scroll.js
@@ -0,0 +1,54 @@
+
+$("#contact_btn2"  ).click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#work").offset().top
+      },
+      1000
+    );
+  });
+
+$("#home_btn").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#home").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#home_btn2").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#home").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#about_btn").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#about").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#work_btn").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#work").offset().top
+      },
+      1000
+    );
+  });
+
+  $("#skills_btn").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#skills").offset().top
+      },
+      1000
+    );
+  });
